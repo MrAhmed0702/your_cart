df96ec62f164f91ede3d795cbc8841bcedf1fdd2
Ahmed Mohammed Jayanti Mochi <amochi670@rku.ac.in>
2023-10-13 22:45:10 +0530

    Created a new flutter project named 'Your cart' which is an ecommerce website.

---
+++ lib/Screen/Welcome.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+class Welcome extends StatelessWidget {
+  const Welcome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF5125D2),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(
+              height: 40,
+            ),
+            Text(
+              'Welcome!',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 52,
+                fontFamily: 'Poppins',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 45),
+            SizedBox(
+              height: 300,
+              child: Image.asset(
+                "images/welcome.png",
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'You want \nAuthentic, Here \nyou go!',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Poppins',
+                color: Colors.white,
+                fontSize: 37,
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Find it now, Buy it here',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white70,
+                fontSize: 14,
+              ),
+            ),
+            SizedBox(height: 40),
+            ElevatedButton(
+              onPressed: () {
+                // Add functionality for the Get Started button here
+              },
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 70,
+                  vertical: 7,
+                ),
+                child: Text(
+                  'Get Started',
+                  style: TextStyle(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                foregroundColor: Color(0xFF5125D2),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(12))),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
